2d3d13a20e0d31f18373a500fd3d5b99294f7397
Jeremy Myers <inernestmusic@gmail.com>
2024-10-10 15:46:34 -0400

    Configure static client to be served by default

--- tilemaker/settings.py
+++ tilemaker/settings.py
@@ -4,7 +4,6 @@ Settings for the project.
 
 from pydantic_settings import BaseSettings
 
-
 class Settings(BaseSettings):
     database_url: str = "sqlite:///./test.db"
     "SQLAlchemy-appropriate database URL."
@@ -13,8 +12,9 @@ class Settings(BaseSettings):
     add_cors: bool = False
     "Settings for managng CORS middleware; useful for development."
 
-    static_directory: str | None = None
-    "Static directory to serve the SPA from. If missing, no static is used."
+    static_directory: str | None = "./static"
+    "Static directory to serve the SPA from. Defaults to a version of the simonsobs/tileviewer UI."
+    "See https://github.com/simonsobs/tileviewer"
 
     api_endpoint: str = "./"
     "The location of the API endpoint. Default assumes from the same place as the server."
