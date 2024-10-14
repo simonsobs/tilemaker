cf4b4baaf70cd425f242edb49c465cb689f0f66f
Jeremy Myers <inernestmusic@gmail.com>
2024-10-14 13:33:30 -0400

    Remove static assets and revert static_directory changes

--- tilemaker/settings.py
+++ tilemaker/settings.py
@@ -12,9 +12,8 @@ class Settings(BaseSettings):
     add_cors: bool = False
     "Settings for managng CORS middleware; useful for development."
 
-    static_directory: str | None = "./static"
-    "Static directory to serve the SPA from. Defaults to a version of the simonsobs/tileviewer UI."
-    "See https://github.com/simonsobs/tileviewer"
+    static_directory: str | None = None
+    "Static directory to serve the SPA from. If missing, no static is used."
 
     api_endpoint: str = "./"
     "The location of the API endpoint. Default assumes from the same place as the server."
